81c3ce7db818fe817d142c20dfe752ab44245379
Dara <dara.dandashi@gmail.com>
2020-06-07 14:10:27 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchimg() {
+    return fetch ('https://dog.ceo/api/breeds/image/random/4')
+    .then(Response => Response.json())
+    .then(json => renderBooks(json))
+    
+    }
+    
